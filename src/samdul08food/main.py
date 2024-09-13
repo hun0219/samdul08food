4e209f844560bd7e338ad7db1a36e5306229951f
hun0219 <gsw98000@gmail.com>
2024-09-13 11:25:42 +0900

    index port update

--- src/samdul08food/main.py
+++ src/samdul08food/main.py
@@ -1,7 +1,8 @@
-import pickle
 import os
 from typing import Union
 from fastapi import FastAPI
+import pandas as pd
+import datetime
 
 #with open(get_model_path(), "rb") as f:
 #    fish_model = pickle.load(f)
@@ -14,6 +15,14 @@ def read_root():
 
 @app.get("/food")
 def food(name: str):
-    #시간을 구함
+    # 시간을 구함
     # 음식 이름과 시간을 csv로 저장 -> /code/data/food.csv
-    return {"food": name, "time": "2024-09-15 11:12:13"}
+    # DB 저장
+    dt = datetime.datetime.now().strftime('%Y.%m.%d - %H:%M:%S')
+    home_path = os.path.expanduser('~')
+    path = f"{home_path}/code/data"
+    # 폴더 만들기 있으면 패쓰
+    os.makedirs(path,exist_ok=True)
+    with open(f"{path}/food.csv","a") as f:
+        f.write(f"{dt},{name}\n")
+    return {"food": name, "time": dt}
